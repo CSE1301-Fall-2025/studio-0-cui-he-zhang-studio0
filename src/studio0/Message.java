def1f8fa5b9810bca957c8d749f1435a3f847628
mingxuanZzz <mingxuan.z@wustl.edu>
2025-08-28 03:53:32 +0800

    done

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" Kelvin is stupid ");
+		System.out.println(" Kelvin is good ");
 	}   
 }
